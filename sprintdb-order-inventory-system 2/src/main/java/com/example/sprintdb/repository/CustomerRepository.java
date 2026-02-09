6f7c09cb8a5879b6329f473f7ca724c26c05800e
Sandeep0018-cap <sandeep.sandeep-kumar@capgemini.com>
2026-02-09 16:47:06 +0530

    Delete sprintdb-order-inventory-system 2 directory

--- sprintdb-order-inventory-system 2/src/main/java/com/example/sprintdb/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sprintdb.repository;
-
-import com.example.sprintdb.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
