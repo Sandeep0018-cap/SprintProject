0035f5ae1b6aa18b17dde569f00db122ec5c0da3
Sandeep0018-cap <sandeep.sandeep-kumar@capgemini.com>
2026-02-09 17:26:31 +0530

    Delete BGCOMPLETED directory

--- BGCOMPLETED/src/main/java/com/example/sprintdb/repository/PurchaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sprintdb.repository;
-
-import com.example.sprintdb.entity.Purchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-}
